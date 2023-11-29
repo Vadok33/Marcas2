5dffb1b6853ac9cc09ec5ececcbf65e119ecfcc8
alvaro <a.elenanavarro@gmail.com>
2023-11-29 14:31:10 +0100

    cosas marcas

--- Matrices/matrices.js
+++ Matrices/matrices.js
@@ -1,12 +1,30 @@
-//generar matriz
+// Definir matriz
 let matriz = [
     [0,0,0,0]
     [0,0,0,0]
     [0,0,0,0]
     [0,0,0,0]
-]
+];
 
-//visualizar matriz
+matriz;
+
+
+//Funcion para generar numeros aleatorios
+
+//function numGenerate(max){
+//    return Math.floor(Math.random() * max);
+//    console.log(numGenerate());
+//}
+
+
+
+// Rellenar matriz con números aleatorios   
+
+function rellenarmatriz(matriz){
+    for ()
+}
+
+// Visualizar matriz
 function visualizarMatriz(matriz){
     for (let i=0; i<matriz.length; i++){
         for (let j=0; j<matriz[i].length; j++ ){
@@ -15,6 +33,11 @@ function visualizarMatriz(matriz){
     }
 }
 
-window.onload = function Carga(){
-    visualizarMatriz(matriz);
-}
+
+
+document.getElementById("loo").innerHTML = visualizarMatriz(matriz);
+
+// function Carga(){
+//    visualizarMatriz(matriz);
+//    numGenerate();
+//}
